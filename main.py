5fc7d937947d69e209548cadd5effa1a22a5c6b6
askayo123 <davidaskayo@gmail.com>
2025-11-25 00:21:24 +0200

    getting input of csv

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import os
 
+file_path= 'data\BrandsHatchLayout.csv'
+
+if os.path.exists(file_path):
+    df = pd.read_csv(file_path)
+    print("Data loaded successfully.")  
+    print(df.to_string())
+
+else:
+    print(f"File not found: {file_path}")
